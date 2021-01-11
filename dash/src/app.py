7c51e84edb7784457b34b74ee97a2758af7bd724
SvenHosp <Sven.Hosp@deutschebahn.com>
2021-01-11 17:54:16 +0100

    add user logic

--- dash/src/app.py
+++ dash/src/app.py
@@ -9,8 +9,8 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True)
 global card_game
 card_game = game.game_engine()
 
-bg_color = '#ad0000'
-
+global current_user
+current_user = None
 
 def trigger_prop_to_dict(trigger_prop):
     import json
@@ -29,46 +29,80 @@ def trigger_prop_to_dict(trigger_prop):
 
 def generate_card_representations():
     container_list = []
-    card_to_game = []
     i = 0
-    for _card in card_game.get_card_list():
-        container_list.append(
-            dcc.Textarea(
-                id={
-                    'type': 'card_Textarea',
-                    'index': i,
-                    'card_value': _card.value,
-                    'bg_color': _card.bg_color
-                },
-                value=str(_card.value),
-                style={'width': 50, 'height': 200, 'backgroundColor': _card.bg_color, 'color': _card.text_color, 'textAlign': 'center', 'fontSize': 20},
-                readOnly=True,
-                n_clicks=0,
+    if card_game.get_card_list(current_user) is not None:
+        print(current_user)
+        for _card in card_game.get_card_list(current_user):
+            container_list.append(
+                dcc.Textarea(
+                    id={
+                        'type': 'card_Textarea',
+                        'index': i,
+                        'card_value': _card.value,
+                        'bg_color': _card.bg_color
+                    },
+                    value=str(_card.value),
+                    style={'width': 50, 'height': 200, 'backgroundColor': _card.bg_color, 'color': _card.text_color, 'textAlign': 'center', 'fontSize': 20},
+                    readOnly=True,
+                    n_clicks=0,
+                )
             )
-        )
-        i = i + 1
+            i = i + 1
     return container_list
 
 app.layout = html.Div([
+    html.Div(id='user-formular', children=[
+        dcc.Input(
+            id='input-box',
+            placeholder='Enter your name...',
+            type='text',
+            value=''
+        ),
+        html.Button('Submit', id='button', n_clicks=0),
+        html.Div(id='output-container-button',
+             children='Enter your name and press submit')
+    ]),
+    html.Div(id='management-formular', children=[
+        html.Button('start game', id='button_start_game', n_clicks=0),
+    ]),
     html.Div(id='dynamic-cards-container', children=generate_card_representations())
 ])
 
 @app.callback(
+    Output('output-container-button', 'children'),
+    [Input('button', 'n_clicks')],
+    [State('input-box', 'value')])
+def create_user(n_clicks, value):
+    if n_clicks >= 1:
+        card_game.create_user(value)
+        card_game.distribute_cards()
+        global current_user
+        current_user=value
+        return 'user created!'
+
+
+@app.callback(
     Output('dynamic-cards-container', 'children'),
-    Input({'type': 'card_Textarea', 'index': ALL, 'card_value': ALL, 'bg_color': ALL}, 'n_clicks'),
+    [
+        Input({'type': 'card_Textarea', 'index': ALL, 'card_value': ALL, 'bg_color': ALL}, 'n_clicks'),
+        Input('button_start_game', 'n_clicks')
+    ],
     State({'type': 'card_Textarea', 'index': ALL, 'card_value': ALL, 'bg_color': ALL}, 'value')
 )
-def update_output(n_clicks, value):
+def update_output(n_clicks1, n_clicks2, value):
     # n_clicks is a list!!!!! of elements
     trigger = None
     for t in dash.callback_context.triggered:
         trigger = t
         continue
-
-    if trigger['value'] is not None:
-        trigger_dict = trigger_prop_to_dict(trigger)
     
-        card_game.remove_card(trigger_dict['card_value'], trigger_dict['bg_color'])
+    if trigger['value'] is not None:
+        if trigger['prop_id'].find('button_start_game') == -1:
+            trigger_dict = trigger_prop_to_dict(trigger)
+    
+            card_game.remove_card(current_user, trigger_dict['card_value'], trigger_dict['bg_color'])
+        else:
+            print(trigger)
 
     return generate_card_representations()
 
--- dash/src/game.py
+++ dash/src/game.py
@@ -4,12 +4,15 @@ class card():
         self.bg_color = '#ad0000'
         self.text_color = '#ffffff'
 
-class game_engine():
-    def __init__(self) -> None:
+class user():
+    def __init__(self, name) -> None:
         super().__init__()
+        self.name = name
         self.card_list = []
-        for i in range(6):
-            self.card_list.append(card(str(i+1)))
+
+    def push_cards(self, card_list):
+        for _card in card_list:
+            self.card_list.append(_card)
 
     def get_card_list(self):
         return self.card_list
@@ -30,3 +33,46 @@ class game_engine():
                 __card = _card
                 continue
         self.card_list.remove(__card)
+
+class game_engine():
+    def __init__(self) -> None:
+        super().__init__()
+        self.user_list = []
+        self.card_stack_list = []
+        for i in range(6):
+            self.card_stack_list.append(card(str(i+1)))
+    
+    def create_user(self, name):
+        self.user_list.append(user(name))
+    
+    def get_user(self, name):
+        for _user in self.user_list:
+            if _user.name == name:
+                return _user
+    
+    def distribute_cards(self):
+        for _user in self.user_list:
+            _user.push_cards(self.card_stack_list)
+
+    def get_card_list(self, username):
+        for _user in self.user_list:
+            if username == _user.name:
+                return _user.get_card_list()
+    
+    def get_card(self, username, value, bg_color):
+        for _user in self.user_list:
+            if username == _user.name:
+                return _user.get_card(value, bg_color)
+
+
+    def remove_card(self, username, _card):
+        for _user in self.user_list:
+            if username == _user.name:
+                _user.remove_card(_card)
+    
+    def remove_card(self, username, value, bg_color):
+        for _user in self.user_list:
+            if username == _user.name:
+                _user.remove_card(value, bg_color)
+
+
